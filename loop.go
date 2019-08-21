88e9dc9788419f38d61db2d14984c756153c391c
0xaeeeee <hkdjcmc@msn.cn>
2019-08-21 23:11:02 +0800

    Update loop.go

--- loop.go
+++ loop.go
@@ -7,9 +7,12 @@ import (
 
 func convertToBin(n int) string {
 	result := ""
+	if n == 0 {
+		return "0"
+	}
 	for ; n > 0; n /= 2 {
-		lsb := n%2
-		result= strconv.Itoa(lsb) + result
+		lsb := n % 2
+		result = strconv.Itoa(lsb) + result
 	}
 	return result
 }
@@ -18,5 +21,6 @@ func main() {
 	fmt.Println(
 		convertToBin(5),
 		convertToBin(13),
-		)
+		convertToBin(0),
+	)
 }
