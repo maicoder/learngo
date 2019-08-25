9157de874f7dbe2b821d4f307da05097dfb98e17
0xaeeeee <hkdjcmc@msn.cn>
2019-08-25 21:31:36 +0800

    Update slices.go

--- slices.go
+++ slices.go
@@ -39,4 +39,10 @@ func main() {
 	s2 = s1[3:5]
 	fmt.Printf("s1=%v, len(s1)=%d, cap(s1)=%d", s1, len(s1), cap(s1))
 	fmt.Printf("s2=%v, len(s2)=%d, cap(s2)=%d", s2, len(s2), cap(s2))
+
+	s3 := append(s2, 10)
+	s4 := append(s3, 11)
+	s5 := append(s4, 12)
+	fmt.Println("s3, s4, s5=", s3, s4, s5)
+	fmt.Println("arr = ", arr)
 }
