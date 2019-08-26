c76f5e1801fda045b6a660440ba528fa821c77ae
0xaeeeee <hkdjcmc@msn.cn>
2019-08-26 23:40:51 +0800

    Update sliceops.go

--- sliceops.go
+++ sliceops.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func printSlice(s []int) {
-	fmt.Printf("len=%d, cap=%d\n", len(s), cap(s))
+	fmt.Printf("%v, len=%d, cap=%d\n", s, len(s), cap(s))
 }
 
 func main() {
@@ -14,4 +14,12 @@ func main() {
 		s = append(s, 2*i+1)
 	}
 	fmt.Println(s)
+
+	s1 := []int{2, 4, 6, 8}
+	printSlice(s1)
+
+	s2 := make([]int, 16)
+	s3 := make([]int, 10, 32)
+	printSlice(s2)
+	printSlice(s3)
 }
