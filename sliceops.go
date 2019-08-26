bd61678f8f3ed5bbdb91e66151bfb28bf46de35a
0xaeeeee <hkdjcmc@msn.cn>
2019-08-26 23:46:12 +0800

    Update sliceops.go

--- sliceops.go
+++ sliceops.go
@@ -7,6 +7,7 @@ func printSlice(s []int) {
 }
 
 func main() {
+	fmt.Println("Creating slice")
 	var s []int // Zero value for slice is nil
 
 	for i := 0; i < 100; i++ {
@@ -22,4 +23,14 @@ func main() {
 	s3 := make([]int, 10, 32)
 	printSlice(s2)
 	printSlice(s3)
+
+	fmt.Println("Copying slice")
+	copy(s2, s1)
+	printSlice(s2)
+
+	fmt.Println("Deleting elements from lice")
+	s2 = append(s2[:3], s2[4:]...)
+	printSlice(s2)
+
+	
 }
