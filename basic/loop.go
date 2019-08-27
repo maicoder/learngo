09a119f6c2a9f4d3b6faa6195416f7584cb4825a
0xaeeeee <hkdjcmc@msn.cn>
2019-08-27 16:07:53 +0800

    Create loop.go

---
+++ basic/loop.go
@@ -0,0 +1,48 @@
+package basic
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func convertToBin(n int) string {
+	result := ""
+	if n == 0 {
+		return "0"
+	}
+	for ; n > 0; n /= 2 {
+		lsb := n % 2
+		result = strconv.Itoa(lsb) + result
+	}
+	return result
+}
+
+func printFile(filename string)  {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
+
+func forever()  {
+	for {
+		fmt.Println("abc")
+	}
+}
+
+func main() {
+	fmt.Println(
+		convertToBin(5),
+		convertToBin(13),
+		convertToBin(0),
+	)
+	printFile("abc.txt")
+	forever()
+}
