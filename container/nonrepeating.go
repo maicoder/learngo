b729166ec3204f17aad60db58dee713381f2e835
0xaeeeee <hkdjcmc@msn.cn>
2019-08-29 11:37:01 +0800

    Update nonrepeating.go

--- container/nonrepeating.go
+++ container/nonrepeating.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 func lengthOfNonRepeatingSubStr(s string) int {
-	lastOccurred := make(map[byte]int)
+	lastOccurred := make(map[rune]int)
 	start := 0
 	maxLength := 0
 
-	for i, ch := range []byte(s) {
+	for i, ch := range []rune(s) {
 		if lastI, ok := lastOccurred[ch]; ok && lastI >= start {
 			start = lastI + 1
 		}
