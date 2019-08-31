56915882bc203b6eaa7188f3810e5d8a4b7f8d90
0xaeeeee <hkdjcmc@msn.cn>
2019-08-31 11:07:07 +0800

    Create basic.go

---
+++ basic/basic/basic.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/cmplx"
+)
+
+var (
+	aa = 3
+	bb = true
+	ss = "kkk"
+)
+
+func variableZeroValue() {
+	var a int
+	var s string
+	fmt.Printf("%d %q\n", a, s)
+}
+
+func variableInitialValue() {
+	var a, b int = 3, 4
+	var s string = "abc"
+	fmt.Println(a, b, s)
+}
+
+func variableTypeDeduction() {
+	var a, b, c, s = 3, 4, true, "def"
+	fmt.Println(a, b, c, s)
+}
+
+func variableShorter() {
+	a, b, c, s := 3, 4, true, "def"
+	b = 5
+	fmt.Println(a, b, c, s)
+}
+
+// 	欧拉
+func euler() {
+	//c := 3 + 4i
+	//fmt.Println(cmplx.Abs(c))
+
+	fmt.Printf("%.3f\n", cmplx.Exp(1i*math.Pi)+1)
+}
+
+func triangle() {
+	var a, b float64 = 3.0000, 4.0000
+	var c float64
+	c = math.Sqrt(a*a + b*b)
+	fmt.Println(a * a)
+	fmt.Println(c)
+}
+
+func consts() {
+	const filename = "abc.txt"
+	const a, b = 3, 4
+	var c int
+	c = int(math.Sqrt(a*a + b*b))
+	fmt.Println(filename, c)
+}
+
+func enums() {
+	// 普通枚举类型
+	const (
+		cpp = iota
+		_
+		java
+		python
+		golang
+	)
+
+	//自增值枚举类型
+	// b, kb, mb, gb, tb, pb
+	const (
+		b = 1 << (10 * iota)
+		kb
+		mb
+		gb
+		tb
+		pb
+	)
+
+	fmt.Println(cpp, java, python, golang)
+	fmt.Println(b, kb, mb, gb, tb, pb)
+}
+
+func main() {
+	fmt.Println("Hello World")
+	variableZeroValue()
+	variableInitialValue()
+	variableTypeDeduction()
+	variableShorter()
+	fmt.Println(aa, bb, ss)
+
+	euler()
+
+	triangle()
+
+	consts()
+	enums()
+}
