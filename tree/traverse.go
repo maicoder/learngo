3481da8032569e11423f5279e64311af5ece6b29
0xaeeeee <hkdjcmc@msn.cn>
2019-09-03 00:18:41 +0800

    Update traverse.go

--- tree/traverse.go
+++ tree/traverse.go
@@ -1,11 +1,27 @@
 package tree
 
+import "fmt"
+
 // 遍历
 func (node *Node) Traverse() {
+	//if node == nil {
+	//	return
+	//}
+	//node.Left.Traverse()
+	//node.Print()
+	//node.Right.Traverse()
+
+	node.TraverseFunc(func(n *Node) {
+		n.Print()
+	})
+	fmt.Println()
+}
+
+func (node *Node) TraverseFunc(f func(*Node)) {
 	if node == nil {
 		return
 	}
-	node.Left.Traverse()
-	node.Print()
-	node.Right.Traverse()
+	node.Left.TraverseFunc(f)
+	f(node)
+	node.Right.TraverseFunc(f)
 }
