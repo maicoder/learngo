c98f368f622a2207ac74a0d82be050eeadc22787
0xaeeeee <hkdjcmc@msn.cn>
2019-09-03 00:01:49 +0800

    Update fib.go

--- functional/fib.go
+++ functional/fib.go
@@ -1,8 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strings"
+)
 
-func fibonacci() func() int {
+func fibonacci() intGen {
 	a, b := 0, 1
 	return func() int {
 		a, b = b, a+b
@@ -10,18 +15,26 @@ func fibonacci() func() int {
 	}
 }
 
+type intGen func() int
+
+func (g intGen) Read(p []byte) (n int, err error) {
+	next := g()
+	if next > 10000 {
+		return  0, io.EOF
+	}
+	s := fmt.Sprintf("%d\n", next)
+	return strings.NewReader(s).Read(p)
+}
+
+func printFileContents(reader io.Reader)  {
+	scanner := bufio.NewScanner(reader)
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
+
 func main() {
 	f := fibonacci()
-
-	fmt.Println(f())
-	fmt.Println(f())
-	fmt.Println(f())
-	fmt.Println(f())
-	fmt.Println(f())
-	fmt.Println(f())
-	fmt.Println(f())
-	fmt.Println(f())
-	fmt.Println(f())
-	fmt.Println(f())
-
+	printFileContents(f)
 }
