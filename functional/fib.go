46dfc3bf56ca47996c169014afe6cc54146ce53c
0xaeeeee <hkdjcmc@msn.cn>
2019-09-04 23:30:06 +0800

    Update fib.go

--- functional/fib.go
+++ functional/fib.go
@@ -20,14 +20,14 @@ type intGen func() int
 func (g intGen) Read(p []byte) (n int, err error) {
 	next := g()
 	if next > 10000 {
-		return  0, io.EOF
+		return 0, io.EOF
 	}
 	s := fmt.Sprintf("%d\n", next)
 	// TODO: incorrent if p is too small
 	return strings.NewReader(s).Read(p)
 }
 
-func printFileContents(reader io.Reader)  {
+func printFileContents(reader io.Reader) {
 	scanner := bufio.NewScanner(reader)
 
 	for scanner.Scan() {
