769f062171429ee8da0a93d6711cfc603af9427f
0xaeeeee <hkdjcmc@msn.cn>
2019-09-07 00:08:28 +0800

    Update defer.go

--- errhandling/defer/defer.go
+++ errhandling/defer/defer.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"imooc.com/ccmouse/learngo/functional/fib"
+	"os"
+)
 
 func tryDefer() {
 	defer fmt.Println(1)
@@ -10,10 +15,23 @@ func tryDefer() {
 	fmt.Println(4)
 }
 
-func writeFile(filen ame string)  {
-	
+func writeFile(filename string) {
+	file, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	defer writer.Flush()
+
+	f := fib.Fibonacci()
+	for i := 0; i < 20; i++ {
+		fmt.Fprintln(writer, f())
+	}
 }
 
 func main() {
-	tryDefer()
+	//tryDefer()
+	writeFile("fib.txt")
 }
