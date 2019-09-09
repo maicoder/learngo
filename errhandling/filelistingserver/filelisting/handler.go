86476342f4997d5dad176ba8c4529e0aa7706fca
0xaeeeee <hkdjcmc@msn.cn>
2019-09-09 10:32:16 +0800

    Update handler.go

--- errhandling/filelistingserver/filelisting/handler.go
+++ errhandling/filelistingserver/filelisting/handler.go
@@ -10,14 +10,12 @@ func HandleFileList(writer http.ResponseWriter, request *http.Request) error {
 	path := request.URL.Path[len("/list/"):]
 	file, err := os.Open(path)
 	if err != nil {
-		//http.Error(writer, err.Error(), http.StatusInternalServerError)
 		return err
 	}
 	defer file.Close()
 
 	all, err := ioutil.ReadAll(file)
 	if err != nil {
-		//panic(err)
 		return err
 	}
 
