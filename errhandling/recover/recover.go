d6c1222dff8a70248014aab0cdcc509a97c8156b
0xaeeeee <hkdjcmc@msn.cn>
2019-09-12 22:26:50 +0800

    Update recover.go

--- errhandling/recover/recover.go
+++ errhandling/recover/recover.go
@@ -4,15 +4,15 @@ import (
 	"fmt"
 )
 
-func tryRecover()  {
-	 defer func() {
-	 	r := recover()
-		 if err, ok := r.(error); ok {
-			 fmt.Println("Error occurred: ", err)
-		 } else {
-		 	panic(fmt.Sprintf("I don't know what to do : %v", r ))
-		 }
-	 }()
+func tryRecover() {
+	defer func() {
+		r := recover()
+		if err, ok := r.(error); ok {
+			fmt.Println("Error occurred: ", err)
+		} else {
+			panic(fmt.Sprintf("I don't know what to do : %v", r))
+		}
+	}()
 
 	//panic(errors.New("this is an error"))
 
