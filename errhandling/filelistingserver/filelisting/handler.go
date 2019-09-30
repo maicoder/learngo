83a1a1db88ea1a28453c98f0bf9017ea08955d6f
0xaeeeee <hkdjcmc@msn.cn>
2019-09-30 23:07:55 +0800

    Update handler.go

--- errhandling/filelistingserver/filelisting/handler.go
+++ errhandling/filelistingserver/filelisting/handler.go
@@ -21,7 +21,7 @@ func (e userError) Message() string {
 
 func HandleFileList(writer http.ResponseWriter, request *http.Request) error {
 	if strings.Index(request.URL.Path, prefix) != 0 {
-		return userError("path must start " + "with " + prefix )
+		return userError("path must start " + "with " + prefix)
 	}
 	path := request.URL.Path[len(prefix):]
 	file, err := os.Open(path)
