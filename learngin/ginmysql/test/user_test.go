a1700ff65156787939dd421de7e3881ad4065bac
0xaeeeee <hkdjcmc@msn.cn>
2019-10-23 01:00:49 +0800

    Update user_test.go

--- learngin/ginmysql/test/user_test.go
+++ learngin/ginmysql/test/user_test.go
@@ -7,6 +7,7 @@ import (
 	"net/http/httptest"
 	"net/url"
 	"strconv"
+	"strings"
 	"testing"
 )
 
@@ -59,5 +60,18 @@ func TestUserPostFormEmailErrorAndPasswordError(t *testing.T)  {
 	req, _ := http.NewRequest(http.MethodPost, "/user/register", bytes.NewBufferString(value.Encode()))
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded; param=value")
 	router.ServeHTTP(w, req)
-	assert.Equal(t, http.StatusBadRequest, w.Code)
+	assert.Equal(t, http.StatusMovedPermanently, w.Code)
+}
+
+func TestUserLogin(t *testing.T)  {
+	email := "youngxhui@163.com"
+	value := url.Values{}
+	value.Add("email", email)
+	value.Add("password", "1234")
+	w := httptest.NewRecorder()
+	request, _ := http.NewRequest(http.MethodPost, "/user/login", bytes.NewBufferString(value.Encode()))
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded; param=value")
+	router.ServeHTTP(w, request)
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, strings.Contains(w.Body.String(), email), true)
 }
