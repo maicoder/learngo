e1a5af0d8424b94c34da77f8a65d4a5e7b319ed1
0xaeeeee <hkdjcmc@msn.cn>
2019-10-24 12:20:34 +0800

    Update initRouter.go

--- learngin/ginupload/initRouter/initRouter.go
+++ learngin/ginupload/initRouter/initRouter.go
@@ -1,8 +1,11 @@
 package initRouter
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"imooc.com/ccmouse/learngo/learngin/ginupload/handler"
+	"imooc.com/ccmouse/learngo/learngin/ginupload/utils"
 )
 
 func SetupRouter() *gin.Engine {
@@ -10,8 +13,9 @@ func SetupRouter() *gin.Engine {
 
 	router.LoadHTMLGlob("/Users/mac/go/src/imooc.com/ccmouse/learngo/learngin/ginupload/templates/*")
 	//router.LoadHTMLGlob("templates/*")
-	router.Static("/statics", "/Users/mac/go/src/imooc.com/ccmouse/learngo/learngin/ginupload/statics")
 	router.StaticFile("/favicon.ico", "/Users/mac/go/src/imooc.com/ccmouse/learngo/learngin/ginupload/favicon.ico")
+	router.Static("/statics", "/Users/mac/go/src/imooc.com/ccmouse/learngo/learngin/ginupload/statics")
+	router.StaticFS("/avatar", http.Dir(utils.RootPath()+"avatar/"))
 	index := router.Group("/")
 	{
 		index.Any("", handler.Index)
@@ -21,11 +25,9 @@ func SetupRouter() *gin.Engine {
 	{
 		userRouter.POST("/register", handler.UserRegister)
 		userRouter.POST("/login", handler.UserLogin)
+		userRouter.GET("/profile", handler.UserProfile)
+		userRouter.POST("/update", handler.UpdateUserProfile)
 	}
 
 	return router
 }
-
-//func retHelloGinAndMethod(context *gin.Context)  {
-//	context.String(http.StatusOK, "hello gin " + strings.ToLower(context.Request.Method) + " method")
-//}
