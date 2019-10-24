7fe5464ca6c64aa887c21b8d2b06646087bf69bd
0xaeeeee <hkdjcmc@msn.cn>
2019-10-24 12:20:31 +0800

    Update userHandler.go

--- learngin/ginupload/handler/userHandler.go
+++ learngin/ginupload/handler/userHandler.go
@@ -1,14 +1,20 @@
 package handler
 
 import (
-	"github.com/gin-gonic/gin"
-	"imooc.com/ccmouse/learngo/learngin/ginupload/model"
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 	"strconv"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"imooc.com/ccmouse/learngo/learngin/ginupload/model"
+	"imooc.com/ccmouse/learngo/learngin/ginupload/utils"
 )
 
-func UserRegister(context *gin.Context)  {
+func UserRegister(context *gin.Context) {
 	var user model.UserModel
 	if err := context.ShouldBind(&user); err != nil {
 		context.String(http.StatusBadRequest, "输入的数据不合法")
@@ -24,7 +30,7 @@ func UserRegister(context *gin.Context)  {
 	context.Redirect(http.StatusMovedPermanently, "/")
 }
 
-func UserLogin(ctx *gin.Context)  {
+func UserLogin(ctx *gin.Context) {
 	var user model.UserModel
 	if e := ctx.Bind(&user); e != nil {
 		log.Panicln("login 绑定错误", e.Error())
@@ -34,12 +40,12 @@ func UserLogin(ctx *gin.Context)  {
 	if u.Password == user.Password {
 		log.Println("登录成功", u.Email)
 		ctx.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"email":u.Email,
+			"email": u.Email,
 		})
 	}
 }
 
-func UserProfile(ctx *gin.Context)  {
+func UserProfile(ctx *gin.Context) {
 	id := ctx.Query("id")
 	var user model.UserModel
 	i, err := strconv.Atoi(id)
@@ -54,14 +60,15 @@ func UserProfile(ctx *gin.Context)  {
 	})
 }
 
-func UpdateUserProfile(ctx *gin.Context)  {
+func UpdateUserProfile(ctx *gin.Context) {
 	var user model.UserModel
 	if err := ctx.ShouldBind(&user); err != nil {
 		ctx.HTML(http.StatusOK, "error.tmpl", gin.H{
-			"errpr": err.Error()
+			"errpr": err,
 		})
 		log.Panicln("绑定发生错误", err.Error())
 	}
+
 	file, e := ctx.FormFile("avatar-file")
 	if e != nil {
 		ctx.HTML(http.StatusOK, "error.tmpl", gin.H{
@@ -69,5 +76,34 @@ func UpdateUserProfile(ctx *gin.Context)  {
 		})
 		log.Panicln("文件上传错误", e.Error())
 	}
-	path := utils.Pa
+
+	path := utils.RootPath()
+	path = filepath.Join(path, "avatar")
+	e = os.MkdirAll(path, os.ModePerm)
+	if e != nil {
+		ctx.HTML(http.StatusOK, "error.tmpl", gin.H{
+			"error": e,
+		})
+		log.Panicln("无法创建文件夹", e.Error())
+	}
+
+	fileName := strconv.FormatInt(time.Now().Unix(), 10) + file.Filename
+	e = ctx.SaveUploadedFile(file, path+fileName)
+	if e != nil {
+		ctx.HTML(http.StatusOK, "error.tmpl", gin.H{
+			"error": e,
+		})
+		log.Panicln("无法保存文件", e.Error())
+	}
+
+	avatarUrl := "http://127.0.0.1:8080/avatar/" + fileName
+	user.Avatar = sql.NullString{String: avatarUrl}
+	e = user.Update(int(user.Id))
+	if e != nil {
+		ctx.HTML(http.StatusOK, "error.tmpl", gin.H{
+			"error": e,
+		})
+		log.Panicln("数据无法更新", e.Error())
+	}
+	ctx.Redirect(http.StatusMovedPermanently, "/user/profile?id="+strconv.Itoa(int(user.Id)))
 }
