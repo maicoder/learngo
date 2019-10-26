7b6c80407b50a218200f44cee6d30209a3558b5a
0xaeeeee <hkdjcmc@msn.cn>
2019-10-26 10:45:19 +0800

    Create Auth.go

---
+++ learngin/gincookie/middleware/Auth.go
@@ -0,0 +1,19 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func Auth() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		cookie, e := context.Request.Cookie("user_cookie")
+		if e == nil {
+			context.SetCookie(cookie.Name, cookie.Value, 1000, cookie.Path, cookie.Domain, cookie.Secure, cookie.HttpOnly)
+			context.Next()
+		} else {
+			context.Abort()
+			context.HTML(http.StatusUnauthorized, "401.tmpl", nil)
+		}
+	}
+}
